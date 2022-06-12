a3a9b0dcd4444e6a0b0d297faf5da3635bc6dc3f
kalpathathsara <kalpathathsara@gmail.com>
2022-06-12 12:49:25 +0530

    Created the login module

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -19,3 +19,23 @@ exports.register = (req, res, results) => {
     });
   });
 };
+
+exports.login = (req, res, results) => {
+  const data = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    emailId: req.body.emailId,
+    password: req.body.password,
+  };
+
+  usersService.login(data, (error, results) => {
+    if (error) {
+      console.log(error);
+      return res.status(400).send({ success: 0, data: "Bad Request" });
+    }
+    return res.status(201).send({
+      success: 1,
+      data: results,
+    });
+  });
+};
--- routes/users.route.js
+++ routes/users.route.js
@@ -54,6 +54,42 @@ router.post("/register", usersController.register);
  *              description: Bad request
  */
 
-//router.post("/login", usersController.login);
+router.post("/login", usersController.login);
+
+/**
+ * @swagger
+ * /users/login:
+ *  post:
+ *      description: Use to login a user
+ *      tags:
+ *          - users
+ *      parameters:
+ *          - in: body
+ *            name: User
+ *            description: User data
+ *            schema:
+ *              type: object
+ *              required:
+ *                  -emailId
+ *                  -password
+ *              properties:
+ *                  emailId:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 100
+ *                      example: navin@sample.com
+ *                  password:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 45
+ *                      example: abcd
+ *      responses:
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 
 module.exports = router;
