0cdc41129ac7ded6bae79c17e3b5b139de093d88
kalpathathsara <kalpathathsara@gmail.com>
2022-06-12 21:34:48 +0530

    Created Add Post

---
+++ services/posts.service.js
@@ -0,0 +1,14 @@
+const db = require("../config/db.config");
+
+exports.addPost = (data, callback) => {
+  db.query(
+    "INSERT INTO posts (description, imagePath, datetimeCreated, addedByUserId) VALUES (?, ?, ?, ?)",
+    [data.description, data.imagePath, new Date(), data.addedByUserId],
+    (error, results, fields) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, "Post Added Successfully");
+    }
+  );
+};
